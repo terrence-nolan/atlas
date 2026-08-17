922ea8028abc3d127df04043a862d0c35cc72f73
Terrence Nolan <terrencenolan@proton.me>
2026-08-17 18:20:06 -0400

    feat(page): force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { getCurrentProject } from "@/features/github/getCurrentProject";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const project = await getCurrentProject();
   console.log(project);
